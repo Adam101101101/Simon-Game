4e6fc577aa1fda6bceb5ad41f03c50cfd1d867c4
Md Yasir <mdyasir9955@gmail.com>
2024-11-02 21:49:47 +0530

    change

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Simon-Game
 it is a game for improvment my skills  
+# add Level
+add the level Features 
--- app.js
+++ app.js
@@ -1,11 +1,72 @@
 let gameSeq= [];
 let userSeq =[];
+let btns =["yellow","red","green","blue"]
 let started = false;
 let p = document.querySelector("p");
+let level =0;
+// let btn = document.querySelector("button")
 document.addEventListener("keypress",function(){
     if(started===false){
         console.log("Game Is Start");
         started = true;
-        p.innerText =  "Level 1"
+        levelUp();
     }
 })
+function userFlash(btn){
+ btn.classList.add("user")
+ setTimeout(function(){
+    btn.classList.remove("user")
+ },200)
+}
+//check Ans Is correct Or not 
+function checkAns(idx){
+    console.log(`current Level Is ${level}`)
+    if(gameSeq[idx]===userSeq[idx]){
+        if(userSeq.length==gameSeq.length){
+            setTimeout(levelUp,500)
+        }
+    }
+    else{
+        p.innerText = `Game Over! Your Score is ${level}! press any key and Start`
+        reset()
+    }
+}
+function btnPress(){
+    let btn = this;
+    userFlash(btn);
+    userSeq.push(btn.getAttribute("id"))
+    console.log(userSeq)
+    checkAns(userSeq.length-1);
+}
+function gameFlash(btn){
+    btn.classList.add("flash")
+    setTimeout(function(){
+        btn.classList.remove("flash")
+    },300)
+    }
+
+function levelUp(){
+    level++;
+    userSeq =[];
+    p.innerText = `level ${level}`
+  // select the random number 
+  let randBtnIndx = Math.floor(Math.random() * 3);
+  let randCol = btns[randBtnIndx];
+  let ranBtn = document.querySelector(`.${randCol}`);
+  gameFlash(ranBtn);
+  gameSeq.push(randCol);
+  console.log(gameSeq)
+
+}
+
+let AllBtn = document.querySelectorAll(".box")
+for(btn of AllBtn){
+   btn.addEventListener("click",btnPress);
+   
+}
+function reset(){
+    started=false;
+    userSeq=[]
+    gameSeq=[]
+    level=0;
+}
